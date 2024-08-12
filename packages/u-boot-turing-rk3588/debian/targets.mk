6165ac80e0620ad30773d7be7e195650f6971e27
Anaple <67675386+Anaple@users.noreply.github.com>
2024-08-12 16:43:43 +0800

    add: lubancat-1

--- packages/u-boot-turing-rk3588/debian/targets.mk
+++ packages/u-boot-turing-rk3588/debian/targets.mk
@@ -11,6 +11,11 @@ orangepi-3b-rk3566_ddr := rk3566_ddr_1056MHz_v1.18.bin
 orangepi-3b-rk3566_bl31 := rk3568_bl31_v1.43.elf
 orangepi-3b-rk3566_pkg := orangepi-3b
 
+u-boot-rockchip_platforms += lubancat-1-rk3566
+lubancat-1-rk3566_ddr := rk3566_ddr_1056MHz_v1.18.bin
+lubancat-1-rk3566_bl31 := rk3568_bl31_v1.43.elf
+lubancat-1-rk3566_pkg := lubancat-1
+
 u-boot-rockchip_platforms += radxa-zero3-rk3566
 radxa-zero3-rk3566_ddr := rk3566_ddr_1056MHz_v1.18.bin
 radxa-zero3-rk3566_bl31 := rk3568_bl31_v1.43.elf
