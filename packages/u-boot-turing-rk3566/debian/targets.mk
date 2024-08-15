e6007dd2bc6b525a2eb402768d286a7c2aa1bfaa
Lin <67675386+Anaple@users.noreply.github.com>
2024-08-15 12:29:57 +0800

    support lubancat-1 uboot

---
+++ packages/u-boot-turing-rk3566/debian/targets.mk
@@ -0,0 +1,7 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+
+u-boot-rockchip_platforms += lubancat-1-rk3566
+lubancat-1-rk3566_ddr := rk3566_ddr_1056MHz_v1.18.bin
+lubancat-1-rk3566_bl31 := rk3568_bl31_v1.43.elf
+lubancat-1-rk3566_pkg := lubancat-1
